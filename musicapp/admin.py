f34e8bc1b340ce9833a208812189877999b93cb6
Thobiy <tobiemmanuel19@gmail.com>
2022-10-30 23:08:14 -0700

    updated the files and added new settings

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Song, Lyric, Artist
 
 # Register your models here.
+admin.site.register(Song)
+admin.site.register(Artist)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    age=models.IntegerField(default='null')
+        
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artist,on_delete=models.CASCADE,null=True)
+    title = models.CharField(max_length=300)
+    date_released = models.DateField(null=True)
+    likes = models.CharField(max_length=300)
+    artist_id = models.CharField(max_length=25, default='null')
+
+
+    def __str__(self):
+        return self.title
+
+    
+class Lyric(models.Model):
+    Artiste = models.ForeignKey(Artist,on_delete=models.CASCADE,null=True)
+    Song = models.ForeignKey(Song,on_delete=models.CASCADE,null=True)
+    content = models.CharField(max_length=3000)
+    song_id = models.CharField(max_length=25, default='null')
+    
+    def __str__(self):
+        if len(self.content) > 100:
+            return f'{self.content[0:100]}...'
+        else:
+            return f'{self.content}'
+            
+
+
